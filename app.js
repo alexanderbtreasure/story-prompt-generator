b83102af30e13ce02cc7fcb8ad53fa3bf9a30d31
alexanderbtreasure <alexanderbtreasure@gmail.com>
2022-06-26 23:14:18 -0400

    First local test.

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const fs = require('fs');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function generate() {
+    const genreData = JSON.parse(fs.readFileSync('example.json'));
+    const genre = genreData[getRandomInt(genreData.length)];
+    console.log(genre);
+}
+
+generate();
